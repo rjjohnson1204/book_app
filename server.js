1f143e7ef4bd2d9fa1e665df3ed2b49dc208f105
mbgoseco <mbgoseco@gmail.com>
2018-12-11 13:53:10 -0800

    working on feature 3 data collection

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
+// Book model
+function Book(item) {
+  this.thumbnail = item.imageLinks.thumbnail;
+  this.title = item.volumeInfo.title;
+  this.author = item.volumeInfo.authors.split(', ');
+  this.description = item.volumeInfo.description;
+}
+
 app.set('view engine', 'ejs');
 
 app.get('/hello', (req, res) => {
@@ -26,6 +34,29 @@ app.get('/', (req, res) => {
   res.render('./pages/index');
 });
 
-app.post('/searches', (req, res) => {
-  console.log('my request body:', req.body);
-});
+app.post('/searches', getResults);
+
+function getResults(req, res) {
+  console.log('line 40: ', req.body);
+  let input = req.body;
+  console.log('line 42: ', input);
+  let results = showResults(input);
+  // res.render('./searches/show', {results: results});
+  console.log('line 45: ', results);
+}
+
+function showResults(input) {
+  console.log('line 49: ', input);
+  console.log('line 49: ', input.search_field);
+  console.log('line 49: ', input.searchby);
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${input.search_field}+${input.searchby}:${input.search_field}`;
+  return superagent.get(url).then(data => {
+    console.log('line 54: ', data.body);
+    const results = data.body.items.map(item => {
+      const book = new Book(item);
+      return book;
+    });
+    console.log('line 59: ', results);
+    return results;
+  });
+}
